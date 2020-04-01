ced1da3b837aa726cf2eb9bb005120630abc141e
unknown <1759654507@qq.com>
2020-04-01 19:10:58 +0800

    update 3

--- problem.py
+++ problem.py
@@ -3,6 +3,21 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
 
+#第三部分：登录成功后页面跳转到学生管理界面及登陆失败提示
+def manage():  #学生管理系统页面
+    top=Tk()
+    top.geometry('280x200')
+    top.title('学生管理系统')
+    Button(top,text='添加成绩',command=add).place(x=100,y=30)
+    Button(top,text='查看成绩',command=check).place(x=100,y=90)
+    Button(top,text='退出系统',command=top.destroy).place(x=100,y=150)
+def defeat():  #登录失败提示
+    messagebox.showerror('登陆失败','用户名或密码错误')
+def access():  #登录成功提示及页面跳转
+    messagebox.showinfo('登陆成功','欢迎，教师%s'%teacher[name.get()]['Name'])
+    top.destroy()
+    manage()
+
 #第二部分：设置初始教师和学生信息并定义login
 teacher={'admin':{'Pwd':'123456','Name':'Admin'},'admin2':{'Pwd':'987654321','Name':'Admin2'}}
 student={'BZB':100,'Bzb':80,'bzb':95}
